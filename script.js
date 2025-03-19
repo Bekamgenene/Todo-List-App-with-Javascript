28b275a0e7352e7eeff2d5c3fde09bd1021d444c
Bekam Genene <bekamgenene@gmail.com>
2025-03-19 20:41:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function addTask() {
   };
 
   const editBtn = document.createElement('button');
-  editBtn.textContent = '✏️';
+  editBtn.textContent = 'Edit';
   editBtn.classList.add('edit-btn');
   editBtn.onclick = function () {
     const newTask = prompt('Edit your task:', taskText);
@@ -33,7 +33,7 @@ function addTask() {
 
   
   const deleteBtn = document.createElement('button');
-  deleteBtn.textContent = '❌';
+  deleteBtn.textContent = 'Delete';
   deleteBtn.classList.add('delete-btn');
   deleteBtn.onclick = function () {
     taskList.removeChild(li);
